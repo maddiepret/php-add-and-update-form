b514e6f75cee3f858f1bce2f010c5c3e9f9e99e3
Madelein Pretorius <50348491+maddiepret@users.noreply.github.com>
2020-06-18 15:31:08 +0200

    Add files via upload
    Made this simple add and update form using PHP and MySQL. I also used Bootstrap for basic styling...

---
+++ includes/database.php
@@ -0,0 +1,17 @@
+<?php
+
+// Connect to database
+$db_host = 'localhost';
+$db_name = 'store';
+$db_user = 'root';
+$db_pass = 'Softlips22';
+
+// Create object to connect
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+// Handle errors
+if (mysqli_connect_errno()) {
+    echo 'This connection failed ' . $mysqli_connect_erro();
+    //cut everything off
+    die;
+}
